1eb6cc5f38021a5305885a7c5b69cbe1162fc3d1
Mateusz Kumpf <mateusz.kumpf@gmail.com>
2019-01-25 13:55:12 +0100

    Add domain layer with entities

---
+++ Patronage2018/Patronage2018.Domain/Entities/Calender.cs
@@ -0,0 +1,18 @@
+﻿using Patronage2018.Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patronage2018.Domain.Entities
+{
+    public class Calender
+    {
+        public int RoomId { get; set; }
+
+        public DateTime From { get; set; }
+
+        public DateTime To { get; set; }
+
+        public Room Room { get; set; }
+    }
+}
---
+++ Patronage2018/Patronage2018.Domain/Entities/Room.cs
@@ -0,0 +1,21 @@
+﻿using Patronage2018.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Patronage2018.Domain.Entity
+{
+    public class Room
+    {
+        public Room()
+        {
+            Calenders = new HashSet<Calender>();
+        }
+
+        public int RoomId { get; set; }
+
+        public string RoomName { get; set; }
+
+        public ICollection<Calender> Calenders { get; private set; }
+    }
+}
