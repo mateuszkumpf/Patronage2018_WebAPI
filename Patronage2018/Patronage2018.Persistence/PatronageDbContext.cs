1784108a91adcec174de38d065933bd8e5559406
Mateusz Kumpf <mateusz.kumpf@gmail.com>
2019-01-25 22:03:11 +0100

    Add persistence layer and DbContext

---
+++ Patronage2018/Patronage2018.Persistence/PatronageDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Patronage2018.Domain.Entities;
+using Patronage2018.Domain.Entity;
+using System;
+
+namespace Patronage2018.Persistence
+{
+    public class PatronageDbContext : DbContext
+    {
+        public PatronageDbContext(DbContextOptions<PatronageDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Room> Rooms { get; set; }
+
+        public DbSet<Calender> Calenders { get; set; }
+    }
+}
