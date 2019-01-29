ff1461cab6713af83e8b66241915bda63c232805
Mateusz Kumpf <mateusz.kumpf@gmail.com>
2019-01-29 22:20:45 +0100

    Add implementation for NotificationService

--- Patronage2018/Patronage2018.Infrastructure/NotificationService.cs
+++ Patronage2018/Patronage2018.Infrastructure/NotificationService.cs
@@ -1,15 +1,47 @@
-﻿using Patronage2018.Application.Interfaces;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Patronage2018.Application.Interfaces;
 using Patronage2018.Application.Notifications.Models;
 using System;
+using System.Net;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace Patronage2018.Infrastructure
 {
     public class NotificationService : INotificationService
     {
-        public Task SendAsync(Message message)
+        private readonly MailSettings _settings;
+
+        public NotificationService(IOptions<MailSettings> settings)
         {
-            throw new NotImplementedException();
+            _settings = settings.Value;
+        }
+
+        public async Task SendAsync(Message message)
+        {
+            var fromAddress = new MailAddress(_settings.From, "From Name");
+            var toAddress = new MailAddress(_settings.To, "To Name");
+
+
+            var smtp = new SmtpClient
+            {
+                Host = _settings.Host,
+                Port = _settings.Port,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                Credentials = new NetworkCredential(fromAddress.Address, _settings.Password),
+                Timeout = 20000
+            };
+
+            using (var mailMessage = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = message.Subject,
+                Body = message.Body
+            })
+            {
+                await smtp.SendMailAsync(mailMessage);
+            }
         }
     }
 }
